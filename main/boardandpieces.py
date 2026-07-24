f7ee529f9e3b09a1fb46fe3771c03ac98888bf2e
AnayTaparia <anay.taparia@gmail.com>
2026-07-24 20:07:25 +0530

    changed board render logic

--- main/boardandpieces.py
+++ main/boardandpieces.py
@@ -11,16 +11,41 @@ def construct_dict(width, height):
     coordict = {}
     for x in range(width):
         for y in range(height):
-            coordict[(x, y)] = None
+            coordict[(x, y)] = Piece(None, None)
     return coordict
 
 class Board:
-    def __init__(self, setup, metadata):
+    def __init__(self, setup, metadata, width, height):
         self.setup = setup
         self.metadata = metadata
+        self.width = width
+        self.height = height
         
     def __getitem__(self, x, y):
         return self.setup[(x, y)]
     
-    def render():
-        pass # TODO: add board rendering in boardrender.py
+    def render(self, conversion):
+        """renders the board. the conversions dictionary needs a Piece(None, None) case.
+
+        Args:
+            conversion (dict): converts pieces to characters
+        """
+        lines = []
+        for y in range(self.height):
+            line = []
+            for x in range(self.height):
+                line.append(conversion[self.setup[(x,y)]])
+            lines.append(" ".join(line))
+        print("\n".join(lines))
+    
+class Piece:
+    def __init__(self, kind, color):
+        self.kind = kind
+        self.color = color
+        
+    def __eq__(self, value):
+        if isinstance(value, Piece):
+            return self.kind == value.kind and self.color == value.color
+        elif isinstance(value, tuple):
+            kind, color = value
+            return self.kind == kind and self.color == color
